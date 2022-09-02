4c94b989270534fd2b131fd59a548b549978d627
YACRUZ <yamatsukino@gmail.com>
2022-09-02 10:34:18 -0500

    create work experience #3

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperienceComponent implements OnInit {
 
+  workExperience : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
+
+    let work1 = {
+            fecha : "2020-2022",
+            ubicacion : "Orizaba, ver",
+            puesto : "CEO",
+            empresa : "Tachiyomi",
+            logros : [
+              { descripcion: "Trabajador en API"},
+              { descripcion: "Algoritmo de busqueda"},
+            ]
+
+        };
+    let work2 = {
+            fecha : "2019-2020",
+            ubicacion : "Orizaba, ver",
+            puesto : "CEO",
+            empresa : "SHUESHIA",
+            logros : [
+              { descripcion: "Redes neuronales"},
+              { descripcion: "Lector reader"},
+            ]
+    };
+
+
+    this.workExperience.push(work1);
+    this.workExperience.push(work2);
   }
 
 }
