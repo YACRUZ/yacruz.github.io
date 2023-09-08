330355be9c537891e91327454fc50810f63077c9
YACRUZ <111380939+YACRUZ@users.noreply.github.com>
2023-09-08 07:16:34 -0600

    Update certificates.component.ts

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -24,9 +24,23 @@ export class CertificatesComponent implements OnInit {
       descripcion: "Recibido por Google developers, en Cursea"
     };
 
+    let cer3 = {
+      fecha: "[2023]",
+      titulo: "Certificado de Python",
+      Link: "https://raw.githubusercontent.com/YACRUZ/Cursos/main/Python/Yahir%20Jesus%20Jacome%20Cogco%20-%20Python.png"
+    };
+    
+    let cer4 = {
+      fecha: "[2023]",
+      titulo: "Certificado de Pandas",
+      Link: "https://raw.githubusercontent.com/YACRUZ/Cursos/main/Pandas/Yahir%20Jesus%20Jacome%20Cogco%20-%20Pandas.png"
+    };
+
 
     this.certificates.push(cer1);
     this.certificates.push(cer2);
+    this.certificates.push(cer3);
+    this.certificates.push(cer4);
   }
 
 }
