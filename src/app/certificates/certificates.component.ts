85005d74cda5e18f0ad3451a853cefcdd5c72ca2
YACRUZ <yamatsukino@gmail.com>
2023-09-19 22:54:43 -0600

    update certificates

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -42,14 +42,14 @@ export class CertificatesComponent implements OnInit {
     let cer5 = {
       fecha: "[2023]",
       titulo: "Certificado de Intro to Machine Learning",
-      descripcion: "Recibido por Kaggle para Pandas",
+      descripcion: "Recibido por Kaggle para Intro Machine Learning",
       link: "https://raw.githubusercontent.com/YACRUZ/Cursos/main/Intro_ML/Yahir%20Jesus%20Jacome%20Cogco%20-%20Intro%20to%20Machine%20Learning.png"
     };
 
     let cer6 = {
       fecha: "[2023]",
       titulo: "Certificado de Intermediate to Machine Learning",
-      descripcion: "Recibido por Kaggle para Pandas",
+      descripcion: "Recibido por Kaggle para Intermediate Machine Learning",
       link: "https://raw.githubusercontent.com/YACRUZ/Cursos/main/Inter_ML/Yahir%20Jesus%20Jacome%20Cogco%20-%20Intermediate%20Machine%20Learning.png"
     };
 
